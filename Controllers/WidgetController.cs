aab43b3a35c306eea4c6a576caed39d345d92f44
Judah McAuley <judah.mcauley@gmail.com>
2013-02-19 21:54:25 -0800

    Adding in skeleton of the widget

---
+++ Controllers/WidgetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SponsorAnAFSer.Controllers
+{
+    public class WidgetController : Controller
+    {
+        //
+        // GET: /Widget/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
